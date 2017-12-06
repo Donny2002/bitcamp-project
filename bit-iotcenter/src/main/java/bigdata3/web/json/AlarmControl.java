a2156c01897063e838c281b210b155c47b303f2e
Donny2002 <higashi2002kr@gmail.com>
2017-12-06 20:42:38 +0900

    '.'

--- bit-iotcenter/src/main/java/bigdata3/web/json/AlarmControl.java
+++ bit-iotcenter/src/main/java/bigdata3/web/json/AlarmControl.java
@@ -1,9 +1,6 @@
 package bigdata3.web.json;
 
 
-import static bigdata3.web.json.JsonResult.STATE_FAIL;
-import static bigdata3.web.json.JsonResult.STATE_SUCCESS;
-
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,30 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import bigdata3.domain.IoTDevice;
-import bigdata3.domain.IoTUser;
-import bigdata3.service.IoTDeviceService;
-import bigdata3.service.IoTUserService;
+import bigdata3.service.DeviceService;
 import bigdata3.util.FcmAgent;
-import bigdata3.util.FcmMessage;
 
 @RestController("json.AlarmControl")
 @RequestMapping("/alarm/json")
 public class AlarmControl {
-  
+
   @Autowired ServletContext application;
-  
-  @Autowired IoTUserService userService;
-  
-  @Autowired IoTDeviceService deviceService;
-  
+
+  //@Autowired IoTUserService userService;
+
+  @Autowired DeviceService deviceService;
+
   @Autowired FcmAgent fcmAgent;
-    
+
   @RequestMapping("change/{serialNo:[\\.\\w]+}")
   public Object change(
       @PathVariable String serialNo,
       @RequestParam String message) throws Exception {
-    
+
+/*
     //=> 제품 번호를 이용하여 장비 정보를 가져온다.
     IoTDevice device = deviceService.get(serialNo);
 
@@ -43,33 +37,36 @@ public class AlarmControl {
     if (device == null) {
       return new JsonResult(STATE_FAIL, "등록되지 않은 장비입니다.");
     }
-    
+
     //=> 장비의 주인이 등록되어 있지 않다면,
     if (device.getUserNo() < 1) {
       return new JsonResult(STATE_FAIL, "장비의 사용자가 설정되지 않았습니다.");
     }
-    
+
     //=> 장비의 사용자 정보를 가져온다.
     IoTUser user = userService.get(device.getUserNo());
-    
+
     //=> 사용자의 토큰 정보가 없으면,
     if (user.getToken() == null) {
-      return new JsonResult(STATE_FAIL, 
+      return new JsonResult(STATE_FAIL,
           "사용자의 토큰 값이 유효하지 않습니다. 앱으로 다시 로그인 하거나, "
           + "앱을 다시 설치하여 로그인 하시기 바랍니다.");
     }
-    
+
     //=> FCM 서버에게 알림 메시지를 보낸다.
     String result = fcmAgent.send(new FcmMessage(
         user.getToken(), //=> to
-        message, //=> message 
+        message, //=> message
         "IoT 경고", //=> title
         "감지장치(" + serialNo + ")에 변화가 발생했습니다.")); //=> text
-    
+
     return new JsonResult(STATE_SUCCESS, result);
+*/
+
+    return null;
   }
-  
- 
+
+
 }
 
 
@@ -84,28 +81,28 @@ public class AlarmControl {
    - 데이터 삭제
      => delete from iot_device;
      => delete from iot_user;
-     
+
 1) 장비 등록
    - reset.py를 실행하여 장비를 재등록한다.
    - 또는 웹 브라우저에서 다음 URL을 직접 실행해도 된다.
      => http://localhost:8080/device/json/reset/시리얼번호
    - DB 확인
      => select * from iot_device;
-     
+
 2) 사용자 등록
    - 웹 브라우저에서 다음 URL을 직접 실행한다.
      => http://localhost:8080/user/json/add?email=이메일&name=이름&password=암호
    - DB 확인
      => select * from iot_user;
-     
+
 3) FCM 서버로부터 알림을 받을 수 있도록 토큰 등록
    - 앱에서 로그인을 수행한다.
    - DB 확인
      => select * from iot_user where email='이메일'
-     
+
 4) 장비의 소유주 설정
    - 웹 브라우저에서 다음 URL을 실행하여 장비를 등록한다.
-     => http://localhost:8080/device/form.html 
+     => http://localhost:8080/device/form.html
      => 로그인할 때 사용한 이메일과 장비의 시리얼번호를 입력한 후 등록한다.
    - DB 확인
      => select * from iot_device;
