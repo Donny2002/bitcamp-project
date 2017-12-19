66fcf9c168df11c384ae0a7e63bb25ee36e8825a
Donny2002 <higashi2002kr@gmail.com>
2017-12-19 18:47:14 +0900

    '.'

--- bit-iot-py/shadow.py
+++ bit-iot-py/shadow.py
@@ -15,8 +15,13 @@ def updateCallback(payload, responseStatus, token):
 
 def getCallback(payload, responseStatus, token):
     print("get 명령 수행 결과!")
-    print(payload)
-    print(responseStatus)
+    # AWSIoT 서버에서 받은 JSON 문자열을 객체로 변환
+    dict = json.loads(message.payload.decode('UTF-8'))
+
+    if responseStatus == "rejected" and dict['code'] == 404:
+        print("Shadow가 존재하지 않습니다.")
+    else:
+        print(dict['state']['desired']['led'])
     print(token)
     print("--------------")
 
